dfbe972af9c9d50c2a5b5290da158333562f41cb
DarkSharpness_XzY <76582120+DarkSharpness@users.noreply.github.com>
2021-05-28 19:30:11 +0800

    Update Treap.h

--- C++_Templates/Data/Treap.h
+++ C++_Templates/Data/Treap.h
@@ -1,5 +1,10 @@
+//This offers you a standard Treap,
+//supporting various value type.
+//Remeber you should offer operator
+//">" "==" for the value type
 #ifndef _Data_Treap_H_
 #define _Data_Treap_H_
+#if __cplusplus >= 201103L
 #include <vector>
 #include <Data/Tstack.h>
 
@@ -14,6 +19,7 @@ typedef int value;
  * @brief A balanced tree structure based on Treap algorithm.\n
  * You must offer the operator "==" and "<" for the value type.
  * 
+ * @tparam value Type you want to keep in the Treap
  * 
  */
 //template  <typename value>
@@ -50,15 +56,20 @@ class Treap
     vector<node> t;  //节点数组.t[0]祭天
     size_t root;
     Tstack <size_t> q;//删除的数据可以重复利用
-    //没有儿子
+
+    //更新size
+    inline void update(const size_t &k)
+    {
+        t[k].siz=t[t[k].lc].siz+t[t[k].rc].siz+t[k].rep;
+    }
     //右节点往上
-    inline void Lrot(size_t &k)
+    inline void Lrot(size_t &k) 
     {
         size_t nex = t[k].rc;
         t[k].rc = t[nex].lc;
         t[nex].lc = k;
         t[nex].siz = t[k].siz;
-        t[k].siz = t[t[k].lc].siz + t[t[k].rc].siz + t[k].rep;
+        update(k);
         k = nex;
     }
     //左节点往上
@@ -68,11 +79,12 @@ class Treap
         t[k].lc = t[nex].rc;
         t[nex].rc = k;
         t[nex].siz = t[k].siz;
-        t[k].siz = t[t[k].lc].siz + t[t[k].rc].siz + t[k].rep;
+        update(k);
         k = nex;
     }
-    void Insert(size_t &k, const value &x)
-    {
+    
+
+    void Insert(size_t &k, const value &x){
         if (none) //空节点,说明需要重新申请
         {
             if(q.empty()){//队列里没有元素了 重新申请
@@ -103,9 +115,10 @@ class Treap
                 Lrot(k);
         }
     }
-    void Erase(size_t &k, const value &x)
-    {
-        if(none) return ;
+   
+
+    void Erase (size_t &k, const value &x) {
+        if(none) throw "not found";
         if(t[k].val==x)//找到了 就是删除这个
         {
             if(t[k].rep>1){                   //超过一个,删一个就可以了
@@ -115,7 +128,7 @@ class Treap
             }
             //不能删除干净,分类讨论
             if(t[k].rc==nullnode||t[k].lc==nullnode){ //只有0/1个儿子,直接继承给父亲
-                //q.push(k);                    //k被删除了,但内存不能浪费
+                q.push(k);                    //k被删除了,但内存不能浪费
                 k=t[k].lc+t[k].rc;         
             }
             else {//有两个儿子,把rand更小的交换,自己下去,直到底部或一边没儿子了
@@ -125,10 +138,12 @@ class Treap
             }
             return;
         }
-        --t[k].siz;
         if(x<t[k].val) Erase(t[k].lc,x);
 	    else Erase(t[k].rc,x); 
+        --t[k].siz;//如果throw了,不会执行
     }
+    
+
     void Multiinsert(size_t &k,const value &x,const size_t &multis)
     {
         if (none) //空节点,说明需要重新申请
@@ -163,39 +178,75 @@ class Treap
     
 
     }
-    void Multierase(size_t &k, const value &x, const size_t &_rep)
+    
+
+    void Multierase (size_t &k, const value &x, const size_t &_rep)
     {
+        if(none) return ;//减掉0个
+        if(t[k].val==x)//找到了 就是删除这个
+        {
+            if(t[k].rep>_rep){                   //超过_rep个,删_rep个就可以了
+                t[k].rep-=_rep;
+                t[k].siz-=_rep;
+                return ;
+            }
+            //不能删除干净,分类讨论
+
+            if(t[k].rc==nullnode||t[k].lc==nullnode){ //只有0/1个儿子,直接继承给父亲
+                q.push(k);                            //k被删除了,但内存不能浪费
+                k=t[k].lc+t[k].rc;
+            }
+            else {//有两个儿子,把rand更小的交换,自己下去,直到底部或一边没儿子了
+                if(t[t[k].lc].rand<t[t[k].rc].rand) Rrot(k); //rand小的在上面
+                else Lrot(k);
+                Multierase(k,x,_rep);
+                update(k);
+            }
+            return;
+        }
+        if(x<t[k].val) Multierase(t[k].lc,x,_rep);
+	    else Multierase(t[k].rc,x,_rep);
+        update(k);   //防止没删除或删多了
+        return ; 
     }
-    size_t rank1(const size_t &k,const value &x)const{
+    
+
+    size_t rank1(const size_t &k,const value  &x)const{
         if(none) return 0;//不存在
         if(t[k].val==x)     return t[t[k].lc].siz+1;
         else if(t[k].val<x) return rank1(t[k].rc,x)+t[k].rep+t[t[k].lc].siz;
         else                return rank1(t[k].lc,x);
     }
-    value rank2(const size_t &k,const size_t &x)const{
+    
+
+    value  rank2(const size_t &k,const size_t &x)const{
         if(none) return 0;
         //cout << x <<endl;
         if(x<=t[t[k].lc].siz)               return rank2(t[k].lc,x);
         else if(x<=t[t[k].lc].siz+t[k].rep) return t[k].val;
         else                                return rank2(t[k].rc,x-t[t[k].lc].siz-t[k].rep);
     }
-    void pre(const size_t &k,const value &x,size_t &ans)const{
+    
+
+    void Pre(const size_t &k,const value &x,size_t &ans)const{
         if(none) return ;
         if(t[k].val<x) 
         {
             ans=k;
-            pre(t[k].rc,x,ans);
+            Pre(t[k].rc,x,ans);
         }
-        else pre(t[k].lc,x,ans);
+        else Pre(t[k].lc,x,ans);
     }
-    void suc(const size_t &k,const value &x,size_t &ans)const{
+    
+
+    void Suc(const size_t &k,const value &x,size_t &ans)const{
         if(none) return ;
         if(x<t[k].val) 
         {
             ans=k;
-            suc(t[k].lc,x,ans);
+            Suc(t[k].lc,x,ans);
         }
-        else suc(t[k].rc,x,ans);
+        else Suc(t[k].rc,x,ans);
     }
 
 public:
@@ -216,7 +267,13 @@ public:
      */
     inline void erase(const value &_X)
     {
-        Erase(root, _X);
+        try{
+            Erase(root, _X);
+        }
+        catch(const char *)
+        {
+            ;
+        }
     }
     /**
      * @brief Insert mutiple identical element to the Treap. 
@@ -229,11 +286,15 @@ public:
         Multiinsert(root,_X,_rep);
     }
     /**
-     * @brief 把元素_X从Treap中删除_rep个
-     * 如果删多了或_X不存在,那么删到没有
+     * @brief Erase multiple _X from Treap.
+     * It will delete to a positive number or none.
      * 
-     * @param _X   删除的元素
-     * @param _rep 删除的次数
+     * @param _X   The element to be deleted.
+     * @param _rep The times it should be removed.
+     * 
+     *  --- 
+     * Note that even if _X doesn't exist in Treap,
+     * no error will occur.
      */
     inline void multierase(const value &_X, const size_t &_rep)
     {
@@ -241,31 +302,57 @@ public:
             return; //0次就是不删除
         Multierase(root, _X, _rep);
     }
+    /**
+     * @brief Find out the rank of the element in Treap.
+     * 
+     * @param _X The element to be ranked.
+     * @return The rank of the element in Treap.
+     * If element not existing,it will be rank as the previous element.
+     */
     inline size_t rankval(const value &_X)const{
         return rank1(root,_X);
     }
-    inline value rankloc(const size_t &_loc)const{
+    /**
+     * @brief Find out the element ranked _loc.
+     * 
+     * @param _loc The _loc of the element.
+     * @return Value of the certain ranked element.
+     */
+    inline value  rankloc(const size_t &_loc)const{
         return rank2(root,_loc);
     }
-    inline value pre(const value &x)const{
-        size_t ans=0;
-        pre(root,x,ans);
-        return t[ans].val;
-    }
-    inline value suc(const value &x)const{
-        size_t ans=t.size();//如果排名是最后一个就返回0
-        suc(root,x,ans);
-        return t[ans].val;
-    }
-    //rank
     /**
-     * @brief 初始化空间,会清空Treap并重新申请,可以减少vector申请内存带来的时间开支
+     * @brief Find the previous element.
      * 
-     * @param _siz 申请的空间大小
+     * @param _X The element to find pre with.
+     * @return Value of the previous element.
+     */
+    inline value  pre(const value &_X)const{
+        size_t ans=0;
+        Pre(root,_X,ans);
+        return t[ans].val;
+    }
+    /**
+     * @brief Find the succeeding element.
+     * 
+     * @param _X The element to find suc with.
+     * @return Value of the succeeding element.
+     */
+    inline value  suc(const value &_X)const{
+        size_t ans=0;//如果排名是最后一个就返回0
+        Suc(root,_X,ans);
+        return t[ans].val;
+    }
+    /**
+     * @brief Reserve enough space for vector to
+     * reduce allocation time cost.
+     * 
+     * @param _size The size required.
     */
-    inline void respace(const size_t &_size)//预留空间
-    {
-        while(!q.empty()) q.pop();
+    inline void reserve(const size_t &_size)
+    {//注意 实际需求size要+1
+        if(!(_size+1)) return ;
+        while(!q.empty()) q.pop();// q占用清空 
         root=0;
         if(t.capacity()==_size+1)
         {
@@ -275,13 +362,16 @@ public:
             return ;
         }
         t.empty();
-        t.shrink_to_fit();
         t.reserve(_size+1);
         t.resize(1);
         t.front().rep=t.front().siz=0;
     }
+    //Shrink to save space.
+    inline void shrink(){
+        t.shrink_to_fit();
+    }
     /**
-     * @brief 构建一个 Treap 对象
+     * @brief Construct an empty Treap.(with only nullnode)
      * 
      */
     Treap()
@@ -299,5 +389,5 @@ public:
 #undef null
 
 }
-
+#endif
 #endif
