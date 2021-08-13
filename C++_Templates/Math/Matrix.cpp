dbec5cd2ca533f26147bc8081487cdd692a6e10f
DarkSharpness_XzY <2040703891@qq.com>
2021-08-13 22:14:55 +0800

    Update Matrix.cpp

--- C++_Templates/Math/Matrix.cpp
+++ C++_Templates/Math/Matrix.cpp
@@ -1,25 +1,20 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 typedef long long ll;
 
 typedef size_t value;
 //template <typename value>
 class Matrix{
-    private:
-        size_t len,wid;
-        struct array2D{
-            vector <value> v;
-            value * operator [](const size_t &_X)
-            {
-                return &v[_X];
-            }
-            array2D()
-            {
-                v.clear();   
-            }
-        }vec;
-    
-    public:
+
+private:
+    size_t len,wid;
+    vector <value> v;
+public:
+    value * operator [](const size_t &_X)
+    {
+        return &v[_X*len];
+    }
     //Initial with nothing
     Matrix()
     {   
@@ -31,14 +26,26 @@ class Matrix{
     {
         len=_len;
         wid=_wid;
+        v.resize(_len*_wid);
+    }
+    value A()
+    {
+        if(!len||!wid) return ;
+        if(len!=wid) return v[0];
+        return ;    
     }
 };
 
 
 
+Matrix M(3,3);
+
+
 int main()
 {
-   
+    for(int i=0 ; i<3 ; ++i)
+        for(int j=0 ; j<3 ; ++j) 
+            cin>>M[i][j];
 
     return 0;
 }
