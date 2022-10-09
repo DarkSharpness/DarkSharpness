faddf1efc9edce10e48d0c186fa11a28e8115eed
DarkSharpness_XzY <2040703891@qq.com>
2022-10-09 16:53:52 +0800

    null

---
+++ ACMClass/OJ/17/1703.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+template <class T>
+inline void read(T & tmp) {
+    static char ch;
+    while(!isdigit(ch = getchar()));
+    tmp = 0 ;
+    while(isdigit(ch)) {
+        tmp = tmp * 10 + (ch ^ '0');
+        ch = getchar();
+    }
+}
+
+constexpr int N = 1e5 + 16;
+
+struct node {
+    ll  magic;
+    ll  inter;
+    bool operator < (const node & B) const{
+        return magic > B.magic;
+    }
+}t[N];
+
+ll highest = 0;
+
+int cnt = -1;
+
+ll b[70];
+inline bool Linear_Base(ll x) {
+    for(int i = cnt ; i != -1 ; --i) {
+        if(x & (1ll << i)) {
+            if(!b[i]) {
+                b[i] = x;
+                return true;
+            } else {
+                x ^= b[i];
+            }
+        }
+    }
+    return false;//xor till 0
+}
+
+
+
+
+int main() {
+    int n ;
+    read(n);
+    for(int i = 0 ; i < n ; ++i) {
+        read(t[i].inter);
+        read(t[i].magic);
+        highest = max(highest,t[i].inter);
+    }
+    sort(t,t+n);
+    const float db = 0.33333333;
+    
+    while(highest) {
+        ++cnt;
+        highest >>= 1; 
+    }
+    //cout << cnt << endl;
+    ll ans = 0;
+    for(int i = 0 ; i < n ; ++i) 
+        if(Linear_Base(t[i].inter)) {
+            ans += t[i].magic;
+        }
+    printf("%lld",ans);
+    return 0;
+}
