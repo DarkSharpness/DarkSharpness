4d74e5b9186677a3a06511b0852605769eed1ebf
DarkSharpness_XzY <2040703891@qq.com>
2022-10-31 20:14:03 +0800

    Final

--- ACMClass/BigHomework/2/ICPC/Final2.cpp
+++ ACMClass/BigHomework/2/ICPC/Final2.cpp
@@ -96,11 +96,10 @@ class string {
 
     // const unsigned long long *
     using size_cptr = const unsigned long long*;
-    
     public:
 
     /// @brief Array-style index
-    inline constexpr char operator[](size_t index) const{
+    inline constexpr char operator[](size_t index) {
         return c[23-index];
     }
 
@@ -503,4 +502,443 @@ inline int getTeamID(const string &Name) {
 // Public scoreboard of teamID
 // If frozen , it won't change
 std::set <int,compare>   scoreboard;
+}
+
+namespace ICPC{
+
+inline void submit(int ID,int problem,int time,status _st) {
+    teamData[ID].sumbit(problem,time,_st);
+    historyData[ID].update(problem,time,_st);
+}
+
+/// @brief Print out frozen scoreboard
+inline void printFreeze() {
+    scoreboard.clear();
+    for(int i = 0 ; i < teamCount ; ++i) {scoreboard.insert(i);}
+    int rank = 0;
+    for(auto ID : scoreboard) {
+        const team & team1 = teamData[ID];
+        write << teamName[ID]      << ' '
+              << ++rank            << ' '
+              << team1.passCount() << ' '
+              << team1.penalty()   << ' ';
+        for(int i = 1 ; i <= problemCount ; ++i) {
+            if(team1.passP(i) == false) {
+                if(submissions[i][ID].submission_times != 0) {
+                // Freezing state.
+                write << team1.failF(i) << '/'
+                      << submissions[i][ID].submission_times
+                      << ' ';
+                } else { 
+                    // Not freezing + not passed
+                    if(team1.failF(i) == 0) {
+                        write << ". ";
+                    } else {write << team1.failF(i) << ' ';}
+                }
+            } else {
+                write << '+';
+                if(team1.passF(i) > 0) {
+                    write << team1.passF(i);
+                }
+                write << ' ';
+            }
+        }
+        write << '\n';
+    }
+
+}
+
+/// @brief Print out updated scoreboard
+inline void printUpdate() {
+    scoreboard.clear();
+    for(int i = 0 ; i < teamCount ; ++i) {scoreboard.insert(i);}
+    int rank = 0;
+    for(auto ID : scoreboard) {
+        const team & team1 = teamData[ID];
+        write << teamName[ID]      << ' '
+              << ++rank            << ' '
+              << team1.passCount() << ' '
+              << team1.penalty()   << ' ';
+        for(int i = 1 ; i <= problemCount ; ++i) {
+            if(team1.passP(i) == false) {
+                // Not passed
+                
+                if(team1.failF(i) == 0) {
+                    write << ". ";
+                } else {write << team1.failF(i) << ' ';}
+            } else { // Passed
+                write << '+';
+                if(team1.passF(i) > 0) {
+                    write << team1.passF(i);
+                } else {} // Nothing
+                write << ' ';
+            }
+        }
+        write << '\n';
+    }
+}
+
+/// @brief Things to do before starting the game,
+/// Initialize frozenRank, flushedRank, teamName
+/// and scoreboard in O(nlogn)
+void prework() {
+    // update teamCount
+    teamCount = nameSet.size();
+
+    // Reserve space for teamData.
+    // Use it like an array.
+    teamData.resize(teamCount);
+
+    // Reserve space for flushedRank.
+    // Use it like an array.
+    flushedRank.reserve(teamCount);
+    // Initialize scoreboard and flushedRank.
+    for(int i = 0 ; i < teamCount ; ++i) {
+        scoreboard.insert(i);
+        flushedRank.push_back(i+1);
+    }
+
+    // Reserve space for nameSet.
+    teamName.reserve(teamCount);
+    // Initialize teamName
+    for(auto &&it : nameSet) {
+        teamName.push_back(it);
+    }
+
+    // Initialize status_map.
+    status_map['A'] = AC;
+    status_map['W'] = WA;
+    status_map['R'] = RE;
+    status_map['T'] = TL;
+
+    // Initialize submission
+    submissions.resize(problemCount+1);
+    for(int i = 1 ; i <= problemCount ; ++i) {
+        submissions.resize(teamCount);
+    }
+
+    // Initialize historyData.
+    // Just use it like an array.
+    historyData.resize(teamCount);
+}
+
+/// @brief ADDTEAM operation in O(logn)
+void addTeam() {
+    if(gameStart) { // Error occurred, so skip reading
+        skip_string();
+        write << "[Error]Add failed: competition has started.\n";
+        return;
+    }
+
+    #define Name buffer
+    read >> Name;
+
+    if(nameSet.insert(Name).second == false) {
+        // Fail to insert such a name
+        // so there exists an identical name
+        write << "[Error]Add failed: duplicated team name.\n";
+    } else { 
+        // Successful insertion
+        write << "[Info]Add successfully.\n";
+    }
+
+    #undef Name
+}
+
+/// @brief START operation in O(nlogn)
+void start() {
+    skip_string();  // DURATION
+    if(gameStart) { // Error occurred,so skip reading everything
+        skip_string(); // duration
+        skip_string(); // PROBLEM
+        skip_string(); // problemCount
+        write << "[Error]Start failed: competition has started.\n";
+        return;
+    }
+
+    write << "[Info]Competition starts.\n";
+    gameStart = true;
+    read >> duration;
+
+    skip_string();   // PROBLEM
+
+    read >> problemCount;
+
+    prework(); // Doing something beforehand
+}
+
+/// @brief SUBMIT operation in O(logn)
+void submit() {
+    char problem; // The problem to solve
+    char _status; // Status char
+    int  _time;   // Time of submission
+
+    read >> problem;
+    problem ^= 64; // 'A'~'Z' -> 1 ~ 26;
+
+    skip_string(); // BY
+
+    #define Name buffer
+    read >> Name;
+    int ID = getTeamID(Name);
+
+    skip_string(); // WITH
+
+    read >> _status;
+    // The status now.
+    status _st = status_map[int(_status)];
+
+    skip_string();  // Remainder of the status string
+    skip_string();  // AT
+
+    read >> _time;
+
+    if(gameFreeze == true) { // Game freeze state.
+        submit(ID,(int)problem,_time,_st);
+        // If frozon problem(not passed before frozen)
+        if(frozenData[ID].passP(problem) == false) {
+            submissions[problem][ID].submit();
+            if(_st == AC) {submissions[problem][ID].addAC(_time);}
+        }
+    } else { // Game not freeze state.
+        if(teamData[ID].judgeAC(problem,_st) == false) {
+            // No new AC.
+            submit(ID,(int)problem,_time,_st);
+        } else {
+            // With new AC,
+            // so update scoreboard
+            scoreboard.erase(ID);
+            submit(ID,(int)problem,_time,_st);
+            scoreboard.insert(ID);
+            gameChange = true;
+        }
+    }
+
+    #undef Name
+}
+
+/// @brief FLUSH operation in O(n)
+void flush() {
+    write << "[Info]Flush scoreboard.\n";
+
+    if(gameChange == false) {
+        // "The game doesn't make a difference" case.
+        return;
+    }
+
+    // Now update flushed rank
+    gameChange = false;
+    // Update flushed rank
+    int rank = 0;
+    for(auto iter : scoreboard) {
+        flushedRank[iter] = ++rank;
+    }
+}
+
+/// @brief FREEZE operation in O(n)
+void freeze() {
+    if(gameFreeze == true) {
+        // Duplicated freeze operation.
+        write << "[Error]Freeze failed: scoreboard has been frozen.\n";
+        return;
+    }
+
+    
+    for(int i = 1 ; i <= problemCount ; ++i) {
+        submissions[i].clear();
+        submissions[i].resize(teamCount);
+    }
+
+    // Start freezing.
+    gameFreeze = true;
+    write << "[Info]Freeze scoreboard.\n";
+
+    // Update frozon rank.
+    frozenData = teamData;
+}
+
+
+/// @brief SCROLL operation in O(nlogn)
+void scroll() {
+    if(gameFreeze == false) {
+        write << "[Error]Scroll failed: scoreboard has not been frozen.\n";
+        return;
+    }
+
+    write << "[Info]Scroll scoreboard.\n";
+    gameFreeze = false; // Defreeze
+
+    // Go back to pre-frozen state in O(1);
+    teamData.swap(frozenData);
+
+    printFreeze();
+    // The first untested index of a teamName
+    static std::vector <int> first_index;
+    // Initialize first_index
+    first_index.assign(teamName.size(),1);
+
+    while(scoreboard.size() != 1) {
+        auto iter = --scoreboard.end();
+        int ID = *iter;
+        // End of iter.
+        if(teamData[ID].passCount() == frozenData[ID].passCount()) {
+            scoreboard.erase(iter);
+            continue;
+        }
+        for(int i = first_index[ID]; i <= problemCount ; ++i) {
+            if(teamData[ID].passP(i) == false &&
+             frozenData[ID].passP(i) == true) {
+                teamData[ID].change(i,
+                                    submissions[i][ID].first_AC_time,
+                                    frozenData[ID].passF(i));
+                --iter;
+                // If the rank doesn't changing
+                if(compare()(*iter,ID)) {continue;}
+                // Now the rank does change
+                ++iter;
+                scoreboard.erase(iter);
+                iter = ++scoreboard.insert(ID).first;
+                write << teamName[ID]             << ' '
+                      << teamName[*iter]          << ' '
+                      << teamData[ID].passCount() << ' '
+                      << teamData[ID].penalty()   << '\n';
+                first_index[ID] = i + 1;
+                break;
+            }
+        }
+    }
+
+    // Go back to updated state in O(1);
+    teamData.swap(frozenData);
+    printUpdate();
+
+    // Update scoreboard
+    int rank = 0;
+    for(auto iter : scoreboard) {
+        flushedRank[iter] = ++rank;
+    }
+    gameChange = false;
+}
+
+/// @brief QUERY_RANKING O(logn)
+void query_ranking() {
+    #define Name buffer
+    read >> Name;
+    int ID = getTeamID(Name);
+    if(ID == -1) {
+        // Not found
+        write << "[Error]Query ranking failed: cannot find the team.\n";
+    } else {
+        write << "[Info]Complete query ranking.\n";
+        if(gameFreeze) {
+            write << "[Warning]Scoreboard is frozen. ";
+            write << "The ranking may be inaccurate until it were scrolled.\n";
+        }
+        write << Name << " NOW AT RANKING "<< flushedRank[ID] << '\n';
+    }
+    #undef Name
+}
+
+/// @brief QUERY_SUBMISSION O(logn)
+void query_submission() {
+    #define Name buffer
+    read >> Name;
+    int ID = getTeamID(Name);
+    if(ID == -1) {
+        skip_line();
+        write << "[Error]Query submission failed: cannot find the team.\n";
+        return;
+    }
+
+    // team exists
+    write << "[Info]Complete query submission.\n";
+
+    skip_string(); // WHERE
+
+    #define Problem buffer2
+    read >> Problem;
+    int problem = Problem[8] ^ 64; // A ~ Z -> 1 ~ 26;
+    if(Problem[9] != '\0') {
+        // ALL case
+        problem = 0;
+    }
+
+    skip_string(); // AND
+
+    #define STATUS buffer2
+    read >> STATUS;
+    status _st = status_map[(int)STATUS[7]];
+    int _time;
+    if(STATUS[7] == 'A' && STATUS[8] == 'L') {
+        // STATUS  ALL CASE
+        if(problem == 0) {
+            _time = historyData[ID].getALLALL(problem,_st);
+        } else {
+            _time = historyData[ID].getPROBLEMALL(problem,_st);
+        }
+    } else {
+        if(problem == 0) {
+            _time = historyData[ID].getALLSTATUS(problem,_st);
+        } else {
+            _time = historyData[ID].getPROBLEMSTATUS(problem,_st);
+        } // Nothing
+    }
+    if(_time == 0) {
+        write << "Cannot find any submission.\n"; 
+    } else {
+        write << Name               << ' '
+              << char(problem ^ 64) << ' '
+              << status_string[_st] << ' '
+              << _time              << '\n';
+    }
+    #undef Name
+    #undef Problem
+    #undef STATUS
+}
+
+
+/// @brief read in a single command
+bool readCommand() {
+    #define command buffer
+    read >> command;
+    switch(command[1]) {
+        case 'D': // ADDTEAM
+            addTeam();
+            break;
+        case 'T': // START
+            start();
+            break;
+        case 'L': // FLUSH
+            flush();
+            break;
+        case 'R': // FREEZE
+            freeze();
+            break;
+        case 'C': // SCROLL
+            scroll();
+            break;
+        case 'N': // END
+            return false;
+        default:
+            if(command[6] == '\0') { // SUBMIT
+                submit();
+            } else if(command[6] == 'R') { // QUERY_RANKING
+                query_ranking();
+            }else { // QUERY_SUBMISSION
+                query_submission();
+            }
+    }
+    return true;
+    #undef command
+}
+
+} // END of ICPC
+
+
+signed main() {
+    //freopen("ICPC/data/10.in","r",stdin);
+    //freopen("ans.out","w",stdout);
+    while(ICPC::readCommand());
+    ICPC::write << "[Info]Competition ends.\n";
+    return 0;
 }
--- ACMClass/BigHomework/2/ICPC/test.cpp
+++ ACMClass/BigHomework/2/ICPC/test.cpp
@@ -1,18 +1,83 @@
-#include<bits/stdc++.h>
-using namespace std;
-using ll = long long;
-using ull = unsigned long long;
-//#define int lont long
-set <int> s;
-const int N = 1e6;
-int a[N + 100];
-
-signed main() {
+#include "ICPC.h"
+namespace ICPC {
     
-    for(int i = 0 ; i < N ; ++i){
-        a[i] = rand();
-        s.insert(a[i]);
+
+// Mapping a char into number
+int charMap[128];
+// Maximum characters as key.
+int dfn;
+
+class Trie {
+    private:
+    struct node {
+        int nxt[63]; // The index of next node
+        int rank;
+        node () {
+            rank = -1;
+            memset(nxt,-1,sizeof(nxt));
+        }
+    };
+    std::vector <node> nodeData;
+    public:
+    /// @brief Insert a string
+    inline void insert(const string &str,int _rank) {
+        int root  = 0;
+        int len   = 0;
+        int next  = 0;
+        while(str[len] != '\0') {
+            next = nodeData[root].nxt[charMap[(int)str[len]]];
+            if(next == -1) {
+                next = nodeData[root].nxt[charMap[(int)str[len]]] 
+                     = nodeData.size();
+                nodeData.emplace_back();
+            }
+            root = next;
+            ++len;
+        }
+        nodeData[root].rank = _rank;
     }
-        
-    return 0;
+    /// @brief Find the ID of a string
+    inline int find(const string &str) {
+        int root  =  0;
+        int len   =  0;
+        int next  = -1;
+        while(str[len] != '\0') {
+            next = nodeData[root].nxt[charMap[(int)str[len]]];
+            if(next == -1) return -1;
+            root = next;
+            ++len;
+        }
+        // Not found
+        if(next == 0) return -1;
+        else return nodeData[root].rank;
+    }
+    Trie() {
+        nodeData.resize(1);
+    }
+}nameTrie;
+
+
+void prework() {
+    for(int i = 'a' ; i <= 'z' ; ++i) {
+        charMap[i] = ++dfn;
+    }
+    for(int i = 'A' ; i <= 'Z' ; ++i) {
+        charMap[i] = ++dfn;
+    }
+    for(int i = '0' ; i <= '9' ; ++i) {
+        charMap[i] = ++dfn;
+    }
+    charMap['_'] = ++dfn;
 }
+}
+using namespace ICPC;
+
+int main() {
+    prework();
+    read >> buffer;
+    nameTrie.insert(buffer,10);
+    read >> buffer;
+    write << "ID: ";
+    write << nameTrie.find(buffer);
+}
+
