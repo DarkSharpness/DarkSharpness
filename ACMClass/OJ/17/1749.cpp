d181cdfe059eff8e62926712106ceee3d49db4bd
DarkSharpness_XzY <2040703891@qq.com>
2022-11-14 19:54:57 +0800

    NULL

--- ACMClass/OJ/17/1749.cpp
+++ ACMClass/OJ/17/1749.cpp
@@ -2,43 +2,46 @@
 using namespace std;
 
 const auto func = [](const int &n) -> void {
-    bitset <14> mp[14];
-    bitset <14> col;
-    bitset <14 * 2> a;
-    bitset <14 * 2> b;
-    int depth = 0,cur[14];
-    memset(cur,0,sizeof(14));
-    while(true) {
-        #define x cur[depth]
-        #define y depth
-        while(x == n) {
-            
-        }
-        if(depth < 0) break;
-        if(depth == n) { // Output
-            for(int i = 0 ; i < n ; ++i) {
-                for(int j = 0 ; j < n ; ++j) {
-                    if(mp[i][j]) putchar('Q');
-                    else putchar('.');
-                }
+    bitset <20> b,c,d,a[20];
+    int I[20];
+    // 0 : 回溯状态
+    // 1 : 向下递归
+    // 2 : 向下递归(临时)
+    int command = 1;
+    int j = 0;
+    while(j != -1) {
+        if(j == n) {
+            for(int i = 0 ; i < n ; ++i) { 
+                for(int k = 0 ; k < n ; ++k)
+                    putchar(a[k][i] ? 'Q' : '.');
                 putchar('\n');
             }
-            ++cur[--depth];
+            --j;
+            command = 0;
+        }
+        int i = 0;
+        if(!command) {
+            a[I[j]][j] = b[I[j]] = c[I[j]+j] = d[I[j]-j+n] = false;
+            i = I[j] + 1;
+            command = 1;
+        }
+        for(; i < n; ++i) {
+            if(b[i] || c[i+j] || d[i-j+n]) continue;
+            a[i][j] = b[i] = c[i+j] = d[i-j+n] = true;
+            I[j] = i;
+            ++j;
+            command = 2;
             break;
         }
-        
-        do {
-            if(a[x+y] || b[x-y+n])
-
-        } while(cur[depth] <= n);
+        if(command == 2) command = 1;
+        else         --j,command = 0;
     }
-
-
 };
 
+
 int main() {
-   int n;
-   cin >> n;
-   func(n);
-   return 0;
+    int n;
+    cin >> n;
+    func(n);
+    return 0;
 }
