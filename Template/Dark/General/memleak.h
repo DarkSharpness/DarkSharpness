929bb07b0d130cb06d60029cb707e1c5c5ad2dff
DarkSharpness_XzY <2040703891@qq.com>
2023-01-13 16:18:51 +0800

    Upd: memleak
    Fix bugs in new[] and delete nullptr.
    Add some detail information in memory_recorder.

--- Template/Dark/General/memleak.h
+++ Template/Dark/General/memleak.h
@@ -3,45 +3,55 @@
 
 #include <iostream>
 
+
 /* A simple tool to test memory leak and get storage info. */
 struct memory_recorder { 
     static std::size_t allocated;
+    static std::size_t deallocated;
+    static std::size_t allocatedNew;
     static std::size_t ptr_count;
     ~memory_recorder() {
         printf("\n------------------------------\n");
         printf("Space allocated : %llu\n",allocated);
         printf("Pointers leaked : %llu\n",ptr_count);
+        printf("Space leaked from new only : %llu\n",
+                         allocatedNew - deallocated);
         printf( "------------------------------\n" );
     }
 };
 
 static memory_recorder MEMORY_RECORDER;
 
-std::size_t memory_recorder::allocated = 0;
-std::size_t memory_recorder::ptr_count = 0;
+std::size_t memory_recorder::allocated    = 0;
+std::size_t memory_recorder::ptr_count    = 0;
+std::size_t memory_recorder::deallocated  = 0;
+std::size_t memory_recorder::allocatedNew = 0;
 
 void *operator new (size_t sz) {
     void *p = std::malloc(sz);
-    ++memory_recorder::ptr_count;
     memory_recorder::allocated += sz;
+    memory_recorder::allocatedNew += sz;
+    ++memory_recorder::ptr_count;
     return p;
 }
 
 void *operator new [](size_t sz) {
-    std::cout << sz << '\n';
     void *p = std::malloc(sz);
-    ++memory_recorder::ptr_count;
     memory_recorder::allocated += sz;
+    ++memory_recorder::ptr_count;
     return p;
 }
 
-void operator delete(void* p,std::size_t) {
-    --memory_recorder::ptr_count;
+void operator delete(void* p,std::size_t sz) {
+    if(p != nullptr) {
+        memory_recorder::deallocated += sz;
+        --memory_recorder::ptr_count;
+    }
     return std::free(p);
 }
 
 void operator delete[](void *p) {
-    --memory_recorder::ptr_count;
+    if(p != nullptr) { --memory_recorder::ptr_count; }
     return std::free(p);
 }
 
