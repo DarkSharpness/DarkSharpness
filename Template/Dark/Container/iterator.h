c7f76c1a9684b6baa8872ed5b1ef49da1c136387
DarkSharpness_XzY <2040703891@qq.com>
2023-01-23 19:06:55 +0800

    Update iterator.h
    Complete reverse iterator types.

--- Template/Dark/Container/iterator.h
+++ Template/Dark/Container/iterator.h
@@ -5,9 +5,8 @@
 
 namespace dark {
 
-/* Namespace for Random Access Iterators.  */
-namespace RandomAccess {
-
+/* Reverse iterators. */
+namespace ReverseRandomAccess {
 
 template <class T>
 class iterator {
@@ -15,93 +14,214 @@ class iterator {
     T *pointer;
 
   public:
-    ~iterator() = default;
-    iterator(T *rhs = nullptr) : pointer(rhs) {}
-    iterator &operator = (const iterator &rhs) = default;
+    ~iterator() noexcept = default;
+    iterator(T *rhs = nullptr) noexcept : pointer(rhs)  {}
+    iterator &operator = (const iterator &rhs) noexcept = default;
     /* Return the inner pointer. */
-    T *base() const { return pointer; }
-
-    iterator &operator ++ (void) { ++pointer; return *this; }
-    iterator &operator -- (void) { --pointer; return *this; }
-
-    iterator operator ++ (int) { return iterator(pointer++); }
-    iterator operator -- (int) { return iterator(pointer--); }
-
-    iterator &operator += (size_t __n) { pointer += __n; return *this; }
-    iterator &operator -= (size_t __n) { pointer -= __n; return *this; }
-
-    iterator operator - (size_t __n) const { return iterator(pointer - __n); }
-    iterator operator + (size_t __n) const { return iterator(pointer + __n); }
+    T *base() const noexcept { return pointer; }
 
 
-    friend iterator operator + (size_t __n,const iterator &rhs) {
-        return iterator(__n + rhs.pointer);
-    }
-    friend size_t operator - (const iterator &lhs,const iterator &rhs) {
-        return lhs.pointer - rhs.pointer;
-    }
-    friend bool operator == (const iterator &lhs,const iterator &rhs) {
-        return lhs.pointer == rhs.pointer;
-    }
-    friend bool operator != (const iterator &lhs,const iterator &rhs) {
-        return lhs.pointer != rhs.pointer;
-    }
+    iterator &operator ++ (void) noexcept { --pointer; return *this; }
+    iterator &operator -- (void) noexcept { ++pointer; return *this; }
+    iterator operator ++ (int) noexcept { return iterator(pointer--); }
+    iterator operator -- (int) noexcept { return iterator(pointer++); }
 
 
-    T &operator * (void) const { return *pointer; } 
-    T *operator ->(void) const { return  pointer; }
+    iterator &operator += (size_t __n) noexcept { pointer -= __n; return *this; }
+    iterator &operator -= (size_t __n) noexcept { pointer += __n; return *this; }
+    friend iterator operator + (size_t __n,iterator rhs) noexcept 
+    { return iterator(rhs.pointer - __n); }
+    friend iterator operator + (iterator lhs,size_t __n) noexcept 
+    { return iterator(lhs.pointer - __n); }
+    friend iterator operator - (iterator lhs,size_t __n) noexcept 
+    { return iterator(lhs.pointer + __n); }
+    friend size_t operator - (iterator lhs,iterator rhs) noexcept 
+    { return rhs.pointer - lhs.pointer; }
 
-    T &operator [] (size_t __n) const { return pointer[__n]; }
+
+    friend bool operator == (iterator lhs,iterator rhs) noexcept 
+    { return lhs.pointer == rhs.pointer; }
+    friend bool operator != (iterator lhs,iterator rhs) noexcept 
+    { return lhs.pointer != rhs.pointer; }
+
+
+    T &operator * (void) const noexcept { return *pointer; } 
+    T *operator ->(void) const noexcept { return  pointer; }
+    T &operator [] (size_t __n) const noexcept { return pointer[-__n]; }
 };
+
 
 template <class T>
 class const_iterator {
   protected:
     const T *pointer;
+    using iterator = iterator <T>;
 
   public:
     ~const_iterator() = default;
-    const_iterator(const T *rhs = nullptr)  : pointer(rhs) {}
-    const_iterator(const iterator <T> &rhs) : pointer(rhs.base()) {}
-    const_iterator &operator = (const const_iterator &rhs) = default;
-
+    const_iterator(const T *rhs = nullptr)  noexcept : pointer(rhs) {}
+    const_iterator(iterator rhs) noexcept : pointer(rhs.base()) {}
+    const_iterator &operator = (const const_iterator &rhs) noexcept = default;
+    const_iterator &operator = (iterator rhs) 
+    { pointer = rhs.base(); return *this; }
     /* Return the inner pointer. */
-    const T *base() const { return pointer; }
+    const T *base() const noexcept { return pointer; }
 
-    const_iterator &operator ++ (void) { ++pointer; return *this; }
-    const_iterator &operator -- (void) { --pointer; return *this; }
+    const_iterator &operator ++ (void) noexcept { --pointer; return *this; }
+    const_iterator &operator -- (void) noexcept { ++pointer; return *this; }
+    const_iterator operator ++ (int) noexcept { return const_iterator(pointer--); }
+    const_iterator operator -- (int) noexcept { return const_iterator(pointer++); }
 
-    const_iterator operator ++ (int) { return const_iterator(pointer++); }
-    const_iterator operator -- (int) { return const_iterator(pointer--); }
+    const_iterator &operator += (size_t __n) noexcept
+    { pointer -= __n; return *this; }
+    const_iterator &operator -= (size_t __n) noexcept
+    { pointer += __n; return *this; }
+    friend const_iterator operator + (const_iterator lhs,size_t __n) noexcept
+    { return const_iterator(lhs.pointer - __n); }
+    friend const_iterator operator + (size_t __n,const_iterator rhs) noexcept
+    { return const_iterator(rhs.pointer - __n); }
+    friend const_iterator operator - (const_iterator lhs,size_t __n) noexcept
+    { return const_iterator(lhs.pointer + __n); }
 
-    const_iterator &operator += (size_t __n) { pointer += __n; return *this; }
-    const_iterator &operator -= (size_t __n) { pointer -= __n; return *this; }
+    friend size_t operator - (const_iterator lhs,const_iterator rhs) noexcept
+    { return rhs.base() - lhs.base(); }
+    friend size_t operator - (const_iterator lhs,iterator rhs) noexcept
+    { return rhs.base() - lhs.base(); }
+    friend size_t operator - (iterator lhs,const_iterator rhs) noexcept
+    { return rhs.base() - lhs.base; }
 
-    const_iterator operator - (size_t __n) const { return const_iterator(pointer - __n); }
-    const_iterator operator + (size_t __n) const { return const_iterator(pointer + __n); }
+    friend bool operator == (const_iterator lhs,const_iterator rhs) noexcept 
+    { return lhs.base() == rhs.base(); }
+    friend bool operator == (iterator lhs,const_iterator rhs) noexcept 
+    { return lhs.base() == rhs.base(); }
+    friend bool operator == (const_iterator lhs,iterator rhs) noexcept 
+    { return lhs.base() == rhs.base(); }
+    friend bool operator != (const_iterator lhs,const_iterator rhs) noexcept 
+    { return lhs.base() != rhs.base(); }
+    friend bool operator != (iterator lhs,const_iterator rhs) noexcept 
+    { return lhs.base() != rhs.base(); }
+    friend bool operator != (const_iterator lhs,iterator rhs) noexcept 
+    { return lhs.base() != rhs.base(); }
 
-    friend const_iterator operator + (size_t __n,const const_iterator &rhs) {
-        return const_iterator(__n + rhs.pointer);
-    }
-    friend size_t operator - (const const_iterator &lhs,const const_iterator &rhs) {
-        return lhs.pointer - rhs.pointer;
-    }
-    friend bool operator == (const const_iterator &lhs,const const_iterator &rhs) {
-        return lhs.pointer == rhs.pointer;
-    }
-    friend bool operator != (const const_iterator &lhs,const const_iterator &rhs) {
-        return lhs.pointer != rhs.pointer;
-    }
 
-    
     const T &operator * (void) const { return *pointer; } 
     const T *operator ->(void) const { return  pointer; }
-
-    const T &operator [] (size_t __n) const { return pointer[__n]; }
-
+    const T &operator [] (size_t __n) const { return pointer[-__n]; }
 };
 
 
+}
+
+
+/* Namespace for Random Access Iterators.  */
+namespace RandomAccess {
+
+/* Reverse iterator type. */
+template <class T>
+using reverse_iterator = ReverseRandomAccess::iterator <T>;
+/* Const reverse iterator type. */
+template <class T>
+using const_reverse_iterator = ReverseRandomAccess::const_iterator <T>;
+
+template <class T>
+class iterator {
+  protected:
+    T *pointer;
+
+  public:
+    ~iterator() noexcept = default;
+    iterator(T *rhs = nullptr) noexcept : pointer(rhs)  {}
+    iterator &operator = (const iterator &rhs) noexcept = default;
+    /* Return the inner pointer. */
+    T *base() const noexcept { return pointer; }
+
+
+    iterator &operator ++ (void) noexcept { ++pointer; return *this; }
+    iterator &operator -- (void) noexcept { --pointer; return *this; }
+    iterator operator ++ (int) noexcept { return iterator(pointer++); }
+    iterator operator -- (int) noexcept { return iterator(pointer--); }
+
+
+    iterator &operator += (size_t __n) noexcept { pointer += __n; return *this; }
+    iterator &operator -= (size_t __n) noexcept { pointer -= __n; return *this; }
+    friend iterator operator + (size_t __n,iterator rhs) noexcept 
+    { return iterator(rhs.pointer + __n); }
+    friend iterator operator + (iterator lhs,size_t __n) noexcept 
+    { return iterator(lhs.pointer + __n); }
+    friend iterator operator - (iterator lhs,size_t __n) noexcept 
+    { return iterator(lhs.pointer - __n); }
+    friend size_t operator - (iterator lhs,iterator rhs) noexcept 
+    { return lhs.pointer - rhs.pointer; }
+
+
+    friend bool operator == (iterator lhs,iterator rhs) noexcept 
+    { return lhs.pointer == rhs.pointer; }
+    friend bool operator != (iterator lhs,iterator rhs) noexcept 
+    { return lhs.pointer != rhs.pointer; }
+
+
+    T &operator * (void) const noexcept { return *pointer; } 
+    T *operator ->(void) const noexcept { return  pointer; }
+    T &operator [] (size_t __n) const noexcept { return pointer[__n]; }
+};
+
+
+template <class T>
+class const_iterator {
+  protected:
+    const T *pointer;
+    using iterator = iterator <T>;
+
+  public:
+    ~const_iterator() = default;
+    const_iterator(const T *rhs = nullptr)  noexcept : pointer(rhs) {}
+    const_iterator(iterator rhs) noexcept : pointer(rhs.base()) {}
+    const_iterator &operator = (const const_iterator &rhs) noexcept = default;
+    const_iterator &operator = (iterator rhs) 
+    { pointer = rhs.base(); return *this; }
+    /* Return the inner pointer. */
+    const T *base() const noexcept { return pointer; }
+
+    const_iterator &operator ++ (void) noexcept { ++pointer; return *this; }
+    const_iterator &operator -- (void) noexcept { --pointer; return *this; }
+    const_iterator operator ++ (int) noexcept { return const_iterator(pointer++); }
+    const_iterator operator -- (int) noexcept { return const_iterator(pointer--); }
+
+    const_iterator &operator += (size_t __n) noexcept
+    { pointer += __n; return *this; }
+    const_iterator &operator -= (size_t __n) noexcept
+    { pointer -= __n; return *this; }
+    friend const_iterator operator + (const_iterator lhs,size_t __n) noexcept
+    { return const_iterator(lhs.pointer + __n); }
+    friend const_iterator operator + (size_t __n,const_iterator rhs) noexcept
+    { return const_iterator(rhs.pointer + __n); }
+    friend const_iterator operator - (const_iterator lhs,size_t __n) noexcept
+    { return const_iterator(lhs.pointer - __n); }
+    friend size_t operator - (const_iterator lhs,const_iterator rhs) noexcept
+    { return lhs.base() - rhs.base(); }
+    friend size_t operator - (const_iterator lhs,iterator rhs) noexcept
+    { return lhs.base() - rhs.base(); }
+    friend size_t operator - (iterator lhs,const_iterator rhs) noexcept
+    { return lhs.base() - rhs.base(); }
+
+    friend bool operator == (const_iterator lhs,const_iterator rhs) noexcept 
+    { return lhs.base() == rhs.base(); }
+    friend bool operator == (iterator lhs,const_iterator rhs) noexcept 
+    { return lhs.base() == rhs.base(); }
+    friend bool operator == (const_iterator lhs,iterator rhs) noexcept 
+    { return lhs.base() == rhs.base(); }
+    friend bool operator != (const_iterator lhs,const_iterator rhs) noexcept 
+    { return lhs.base() != rhs.base(); }
+    friend bool operator != (iterator lhs,const_iterator rhs) noexcept 
+    { return lhs.base() != rhs.base(); }
+    friend bool operator != (const_iterator lhs,iterator rhs) noexcept 
+    { return lhs.base() != rhs.base(); }
+
+
+    const T &operator * (void) const { return *pointer; } 
+    const T *operator ->(void) const { return  pointer; }
+    const T &operator [] (size_t __n) const { return pointer[__n]; }
+};
 
 }
 
