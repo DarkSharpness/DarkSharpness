9da0e934c2eb7569f14e123a0153e00f26b6fff0
DarkSharpness_XzY <2040703891@qq.com>
2023-03-08 18:07:54 +0800

    Test cpp

--- Template/Homework/Lite_map.cpp
+++ Template/Homework/Lite_map.cpp
@@ -3,7 +3,7 @@
 
 #include <Dark/memleak>
 #include <Dark/inout>
-#include <Dark/Container/map.h>
+#include "Lite_map.h"
 #include <map>
 
 
@@ -13,11 +13,12 @@ inline void func() {
     for(int i = 0 ; i < 1e6 ; ++i) (rand() + rand() + rand());
     auto t0 = clock() - t1;
 
-    std::pair <int,test> p(3,{});
+    sjtu::pair <int,test> p(3);
+
+    // std::pair <int,test> p(3,{});
 
     dark::map <int,test> map;
-    // std::map <int,std::string> t;
-    // std::map <int,std::string> map = std::map <int,std::string>(t);
+    // std::map <int,test> map;
     srand(time(NULL));
 
     t1 = clock();
@@ -27,12 +28,14 @@ inline void func() {
         map.insert(p); /* Debug test passed. */
     }
     dark::write(clock() - t1 - t0,"");
+    for(auto &&iter : map);
 
     t1 = clock();
     for(int i = 0 ; i < 1e6 ; ++i) {
         map.erase(rand() + rand() + rand()); /* Debug test passed. */
     }
     dark::write(clock() - t1 - t0,"");
+    for(auto &&iter : map);
 
     map.clear();
 
@@ -43,11 +46,20 @@ inline void func() {
         map.erase(rand() + rand() + rand()); /* Debug test passed. */
     }
     dark::writeline(clock() - t1 - t0 * 2);
+    for(auto &&iter : map);
 }
 
 signed main() {
-    int n = 1;
-    // freopen("map0.txt","a",stdout);
+    int n = 0;
     while(n--) func();
+    dark::map <int,int> map;
+    // map[1] = 2;
+    // map[2] = 1;
+    sjtu::pair <int,int> p = {1,2};
+    map.insert(p);
+    p.first = 2;
+    map.at(2) = 1;
+    // map.insert(p);
+    for(auto &iter : map) dark::writeline(iter.first,iter.second);
     return 0;
 }
