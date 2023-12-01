726e5a4a1b45d962458cc39b30af48456497f648
DarkSharpness_XzY <2040703891@qq.com>
2023-12-01 17:23:38 +0800

    Style: Just beautify the std code a little.

--- Work/OJ/format/std.h
+++ Work/OJ/format/std.h
@@ -3,12 +3,14 @@
 
 namespace sjtu {
 
-void my_assert(bool cond, const char *msg) {
-    if (!cond) throw std::runtime_error(msg);
-}
+struct format_error final : std::runtime_error {
+    format_error() noexcept : std::runtime_error("Invalid format string!") {}
+    virtual ~format_error() = default;
+};
 
 template <typename ...Args>
 std::string format(std::string_view fmt, Args &&...args) {
+    constexpr auto   my_assert  = [](bool cond) { if (!cond) throw format_error {}; };
     constexpr std::size_t   n   = sizeof...(args);
     constexpr std::size_t npos  = -1;
 
@@ -17,34 +19,30 @@ std::string format(std::string_view fmt, Args &&...args) {
     std::size_t pos = {};
 
     for (const char *str = fmt.data() ; *str ; ++str) {
-        if (*str == '{') {
+        if (*str == '{') { // Find matching '}'
             switch(*++str) {
                 case '{':
                     ret.push_back('{');
                     break;
-
                 case '}':
-                    my_assert(pos < n, "Invalid format string!");
+                    my_assert(pos < n);
                     ret += data[pos++];
                     break;
 
                 default:
                     // pos = 0 or -1 is ok.
-                    my_assert(std::isdigit(*str) && pos + 1 < 2, "Invalid format string!");
+                    my_assert(std::isdigit(*str) && pos + 1 < 2);
                     pos = npos;
-
                     std::size_t idx = 0; // Index of data.
-                    while(std::isdigit(*str)) { idx = idx * 10 + *(str++) - '0'; }
-                    my_assert(*str == '}' && idx < n, "Invalid format string!");
+                    do { idx = idx * 10 + *(str++) - '0'; } while(std::isdigit(*str));
+                    my_assert(*str == '}' && idx < n);
                     ret += data[idx];
             }
         } else {
-            if (*str == '}')
-                my_assert(*++str == '}', "Invalid format string!");
+            if (*str == '}') my_assert(*++str == '}');
             ret.push_back(*str);
         }
     }
-
     return ret;
 }
 
