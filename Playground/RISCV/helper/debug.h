ff14971299ca338dffe7b0c4d243634f68d8e49c
DarkSharpness_XzY <2040703891@qq.com>
2023-12-15 17:10:46 +0800

    Style(RISCV/debug): I hate red. So green.

--- Playground/RISCV/helper/debug.h
+++ Playground/RISCV/helper/debug.h
@@ -25,7 +25,7 @@ struct details {
     template <typename T, typename ...Args>
     details([[maybe_unused]]T &&v,[[maybe_unused]] Args &&...args) {
 #ifdef _DETAIL
-        std::cout << "\033[31m";
+        std::cout << "\033[32m";
         std::cout << v;
         ((std::cout << ' ' << args),...);
         std::cout << "\033[0m\n";
