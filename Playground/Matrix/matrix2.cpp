0bccce2b5f895c07f82049af5c1ba8a530406cf1
DarkSharpness_XzY <2040703891@qq.com>
2023-12-23 16:17:02 +0800

    Upd(Matrix): Testing...

---
+++ Playground/Matrix/matrix2.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize("O3")
+#include <chrono>
+#include <iostream>
+
+const int N = 1000;
+int a[N][N];
+int b[N][N];
+int c[N][N];
+
+signed main() {
+    for (int i = 0 ; i < N ; ++i)
+        for (int j = 0 ; j < N ; ++j)
+            a[i][j] = rand(), b[i][j] = rand();
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0 ; i < N ; ++i)
+        for (int k = 0 ; k < N ; ++k)
+            for (int j = 0 ; j < N ; ++j)
+                c[i][j] += a[i][k] * b[k][j];
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds> (end - start).count() << "ms\n";
+    return 0;
+}
