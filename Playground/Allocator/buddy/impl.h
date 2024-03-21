39490c16919d770f9e208601b3a47d9282e220cb
DarkSharpness_XzY <2040703891@qq.com>
2024-03-21 22:51:15 +0800

    Fix(buddy): Fix the get_index and set_index functions.

--- Playground/Allocator/buddy/impl.h
+++ Playground/Allocator/buddy/impl.h
@@ -29,17 +29,16 @@ inline static offset_t get_bias(void *__ptr) {
 inline static void *set_bias(offset_t __n) {
     return memory_pool + __n;
 }
+/* A local helper function. */
+inline static std::size_t mask(std::size_t __n) { return 1ull << (kBit - 1 - __n); }
 
 inline void *set_index(number_t __n, rank_t __rk) {
-    if (__rk >= kBit) throw std::bad_alloc();
-    const auto __m = kBit - 1 - __rk;
-    return set_bias((__n >> __m) << __rk);
+    return set_bias((__n & (mask(__rk) - 1)) << __rk);
 }
 
 inline number_t get_index(void *__ptr, rank_t __rk) {
-    if (__rk >= kBit) throw std::bad_alloc();
     const auto __m = get_bias(__ptr);
-    return (__m >> __rk) | (1ull << (kBit - 1 - __rk));
+    return (__m >> __rk) | mask(__rk);
 }
 
 inline void forward_list::debug(rank_t __rk) {
@@ -56,6 +55,23 @@ inline void global_allocator::debug() {
         std::cerr << std::format("Free list {}:\n", i);
         list.debug(i);
     }
+
+    // The code below is used for double check in debug.
+    // The output should be identical to the above.
+
+    // std::cerr << "\nRanges available:\n";
+    // for (std::size_t i = 0 ; i < kBit - kAlign ; ++i) {
+    //     rank_t __rk = kBit - 1 - i;
+    //     std::size_t __beg = 1 << i;
+    //     std::size_t __end = 1 << (i + 1);
+    //     std::cerr << std::format("  Rank {}:\n", __rk);
+    //     for (auto j = __beg ; j != __end ; ++j) {
+    //         if (!test(j)) continue;
+    //         auto l = (j - __beg) << __rk;
+    //         auto r = l + (1 << __rk);
+    //         std::cerr << std::format("    [{}, {})\n", l, r);
+    //     }
+    // }
 }
 
 
